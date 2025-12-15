109152787f2f93b4649f015f9698822972e37b00
aadibaniya-DevOps <baniyaaadi@gmail.com>
2025-12-16 04:31:01 +0545

    Update response message with additional personal details

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hi my name is aaditya baniya. I am an aspiring devops engineer and i will succeed");
+  res.end("Hi my name is aaditya baniya. I am an aspiring devops engineer and i will succeed; i live in tarkeshwor. i have 7 members in my family. my parents ,wife, brother,sisterin law and niece");
 });
 
 server.listen(PORT, () => {
